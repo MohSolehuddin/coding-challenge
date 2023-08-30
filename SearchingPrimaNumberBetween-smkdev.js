bb56b21c426836422ae6f69b9a4749c02083c925
MOH. SOLEHUDDIN <97263821+MohSolehuddin@users.noreply.github.com>
2023-08-31 05:18:01 +0700

    update SearchingPrimaNumberBetween-smkdev.js

--- SearchingPrimaNumberBetween-smkdev.js
+++ SearchingPrimaNumberBetween-smkdev.js
@@ -13,10 +13,12 @@ function primaNumberBetween(startRange, endRange){
 function tampung (nilaiRangeSekarang){
   // membuat wadah untuk menyimpan Array
     let wadah = [];
-  for (let i = Math.floor(nilaiRangeSekarang/2); i > startRange; i-- ) {
+  for (let i = Math.floor(nilaiRangeSekarang/2); i > 1; i-- ) {
     // membuah variabel value yang akan menyimpan nilai modulus dari nilai rantang tertentu yang akan kita coba apakah itu bilangan prima atau bukan
     let value = nilaiRangeSekarang%i;
     wadah.push(value);
   }
   return wadah;
 }
+
+primaNumberBetween(100,200)
