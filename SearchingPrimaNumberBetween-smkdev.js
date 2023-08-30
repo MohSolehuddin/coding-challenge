f6f6e6642129f066e95044e3d0820afc9f86dc72
MOH. SOLEHUDDIN <97263821+MohSolehuddin@users.noreply.github.com>
2023-08-31 04:00:02 +0700

    update

--- SearchingPrimaNumberBetween-smkdev.js
+++ SearchingPrimaNumberBetween-smkdev.js
@@ -4,7 +4,13 @@
 let startRange = 10;
 let endRange = 50;
 
-//perulangan pertama untuk mendapatkan tentang nilai yang ingin di cari nilai primanya
+//perulangan pertama untuk mendapatkan rentang nilai yang ingin di cari nilai primanya
 for(let i = startRange; i <= endRange; i++) {
-  
+  // perulangan ke 2 untuk menangani nilai di rentang tertentu
+  for (let j = Math.floor(i/2); j >= startRange; j--; ) {
+    // membuat wadah untuk menyimpan Array
+    let wadah = [];
+    // membuah variabel value yang akan menyimpan nilai modulus dari nilai rantang tertentu yang akan kita coba apakah itu bilangan prima atau bukan
+    wadah.push(value);
+  }
 }
