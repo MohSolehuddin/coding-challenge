921b33ae3ceb63268bab691c9d3c20a52558feb1
MOH. SOLEHUDDIN <97263821+MohSolehuddin@users.noreply.github.com>
2023-08-31 03:05:31 +0700

    Create SearchingPrimaNumberBetween-smkdev.js
    Memberikan variabel rentang awal dan membuat loop untuk rentang

---
+++ SearchingPrimaNumberBetween-smkdev.js
@@ -0,0 +1,10 @@
+// jangan lupa baca README.MD ya juri hehe:)
+
+// membuat variabel untuk menampung tentang awal dan akhir
+let startRange = 10;
+let endRange = 50;
+
+//perulangan pertama untuk mendapatkan tentang nilai yang ingin di cari nilai primanya
+for(let i = startRange; i <= endRange; i++) {
+  
+}
